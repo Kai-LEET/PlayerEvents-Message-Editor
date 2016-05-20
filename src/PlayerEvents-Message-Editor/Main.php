a22ca5175a1373913f6c03e7732a59c70cf4e569
Kirby <goldishkirby@mailinator.com>
2016-05-20 12:03:41 +0800

    Update Main.php

--- src/PlayerEvents-Message-Editor/Main.php
+++ src/PlayerEvents-Message-Editor/Main.php
@@ -5,6 +5,7 @@ namespace PlayerEvents-Message-Editor;
 use pocketmine\plugin\PluginBase;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\event\Listener;
 // more here
 class Main extends PluginBase implements Listener {
  
